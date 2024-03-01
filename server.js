b0a84c43786cf1877e983b68096272424622ca9a
UTKARSH SINGH <87688882+utkarshsingh06@users.noreply.github.com>
2024-03-01 18:27:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Tesseract = require('tesseract.js');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.use(express.json());
 
 // Set storage engine
